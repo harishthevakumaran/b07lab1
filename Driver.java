649a1509bb2b0bfc19ac64bcb245d944221a4d6a
Harish Thevakumaran <hthevakumaran@gmail.com>
2023-10-01 23:19:09 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,16 +1,43 @@
+import java.util.Arrays;
+import java.io.File;
 public class Driver {
-public static void main(String [] args) {
-Polynomial p = new Polynomial();
-System.out.println(p.evaluate(3));
-double [] c1 = {6,0,0,5};
-Polynomial p1 = new Polynomial(c1);
-double [] c2 = {0,-2,0,0,-9};
-Polynomial p2 = new Polynomial(c2);
-Polynomial s = p1.add(p2);
-System.out.println("s(0.1) = " + s.evaluate(0.1));
-if(s.hasRoot(1))
-System.out.println("1 is a root of s");
-else
-System.out.println("1 is not a root of s");
-}
+	public static void main(String [] args) {
+		/*Polynomial p = new Polynomial();
+		System.out.println(p.evaluate(3));
+		double [] c1 = {6,0,0,5};
+		Polynomial p1 = new Polynomial(c1);
+		double [] c2 = {0,-2,0,0,-9};
+		Polynomial p2 = new Polynomial(c2);
+		Polynomial s = p1.add(p2);
+		System.out.println("s(0.1) = " + s.evaluate(0.1));
+		if(s.hasRoot(1))
+		System.out.println("1 is a root of s");
+		else
+		System.out.println("1 is not a root of s");
+		*/
+		double[] coefficients_test1 = {1,2,3};
+		int[]exponents_test1={0,1,2};
+		Polynomial polynomial_test1 = new Polynomial(coefficients_test1,exponents_test1);
+		double[] coefficients_test2 = {4,5};
+		int[] exponents_test2 = {0,1};
+		Polynomial polynomial_test2 = new Polynomial(coefficients_test2, exponents_test2);
+		
+		Polynomial product_poly = polynomial_test1.multiply(polynomial_test2);
+		System.out.println("Coefficient array is : " + Arrays.toString(product_poly.coefficients_poly));
+		System.out.println("Exponent Array is: "+ Arrays.toString(product_poly.exponent));
+		
+		File newFile = new File("polynomial_tester.txt");
+		Polynomial polynomialRead = new Polynomial(newFile);
+		
+		System.out.println("Coefficent Array of Polynomial from text file: " + Arrays.toString(polynomialRead.coefficients_poly));
+		System.out.println("Exponent Array of Polynomial from text file: " + Arrays.toString(polynomialRead.exponent));
+		
+		Polynomial polynomialSaving = new Polynomial(new double[] {3.0,5.0,5.0}, new int[] {9,3,5});
+		String nameofFile = "output_of_polynomial.txt";
+		polynomialSaving.saveToFile(nameofFile);
+		
+		File outputofFile = new File(nameofFile);
+		Polynomial polynomialLoading = new Polynomial(outputofFile);
+	
+	}
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,23 +1,146 @@
 import java.util.Arrays;
+import java.io.File;
+import java.io.FileReader;
+import java.util.Scanner;
+import java.io.FileWriter;
+import java.io.*;
 
 public class Polynomial{
 
 	double[] coefficients_poly;
+	int[] exponent;
 	
 	public Polynomial(){
 		coefficients_poly = new double[1];
+		exponent = new int[1];
 		coefficients_poly[0] = 0.0;
+		exponent[0] = 0;
 	}
 	
+	public Polynomial(double[] poly_initialize, int[] exponent_intialize){
+		this.coefficients_poly = poly_initialize;
+		this.exponent = exponent_intialize;
+		
+		
+	}
 	public Polynomial(double[] poly_initialize){
 		coefficients_poly = new double[poly_initialize.length];
 		for(int i = 0; i < coefficients_poly.length; i++){
 			coefficients_poly[i] = poly_initialize[i];
 		}
+		for(int i = 0; i < coefficients_poly.length; i++){
+			exponent[i] = i;
+		}
+		
+	}
+	
+	public Polynomial(File poly_file){
+		coefficients_poly = new double[0];
+		exponent = new int[0];
+		
+		
+		try(BufferedReader readerObj = new BufferedReader(new FileReader(poly_file))){
+			String lineRead = readerObj.readLine();
+			if(lineRead != null){
+				String[] coefficients_text = lineRead.split("(?=[+-])");
+				coefficients_poly = new double[coefficients_text.length];
+				exponent = new int[coefficients_text.length];
+				for(int i = 0; i <coefficients_text.length; i++){
+					String texts = coefficients_text[i].trim();
+					if(!texts.isEmpty()){
+						String[] index_poly = texts.split("x");
+						double new_coefficient = Double.parseDouble(index_poly[0]);
+						int new_exponent = 0;
+						if(index_poly.length >= 2){
+							new_exponent = Integer.parseInt(index_poly[1]);
+						}
+						coefficients_poly[i] = new_coefficient;
+						exponent[i] = new_exponent;
+					}
+					
+				}
+				
+			}
+			else{
+				coefficients_poly = new double[0];
+				exponent = new int[0];
+				
+			}
+			
+			
+		}
+		catch(IOException exceptionObj){
+			System.err.println("Error from gathering input from file: "+ exceptionObj.getMessage());
+		}
+		
 		
 		
 	}
 	
+	
+	
+	public Polynomial multiply(Polynomial second_poly){
+		int maxExpo1 = this.currentExponent();
+		int maxExpo2 = second_poly.currentExponent();
+		double[] newCoefficient = new double[maxExpo1 + maxExpo2 + 1];
+		int[] newExponents = new int[maxExpo1 + maxExpo2 + 1];
+		for(int i = 0; i<= maxExpo1; i++){
+			for(int j = 0; j <= maxExpo2; j++){
+				double changedCoefficient = this.coefficients_poly[i] * second_poly.coefficients_poly[j];
+				int changedExpo = this.exponent[i] + second_poly.exponent[j];
+				if(newExponents[changedExpo] == 0){
+					newCoefficient[changedExpo] = changedCoefficient;
+					newExponents[changedExpo]= changedExpo;
+				}
+				else{
+					newCoefficient[changedExpo] = newCoefficient[changedExpo] + changedCoefficient;
+				}
+				
+				
+			}
+			
+		}
+		
+		return new Polynomial(newCoefficient, newExponents).polynomialSimplify();
+		
+		
+	}
+	
+	public int currentExponent(){
+		int currentExpo = 0;
+		for (int i = 0; i < exponent.length; i++){
+			if(exponent[i] > currentExpo){
+				currentExpo = exponent[i];
+				
+			}
+			
+		}
+		return currentExpo;
+		
+		
+	}
+	
+	public Polynomial polynomialSimplify(){
+		int changed_size = 0;
+		
+		for(int i = 0; i< coefficients_poly.length; i++){
+			if(coefficients_poly[i] != 0){
+				changed_size = changed_size + 1;
+				
+			}
+			
+		}
+		double[] finalCoefficient = new double[changed_size];
+		int [] finalExpo = new int[changed_size];
+		for(int i = 0; i < changed_size; i++){
+			finalExpo[i] = exponent[i];
+			finalCoefficient[i] = coefficients_poly[i];
+			
+		}
+		return new Polynomial(finalCoefficient, finalExpo);
+		
+		
+	}
 	public Polynomial add(Polynomial second_poly){
 		int length1;
 		int length2;
@@ -34,12 +157,13 @@ public class Polynomial{
 			cpy = second_poly;
 			final_length = length2;
 		}
+		
 		double[] sumCoefficient = new double[final_length];
 		for (int i = 0; i < final_length; i++){
 			double first_co = 0.0;
 			double second_co = 0.0;
 			if( i < length1){
-				first_co = this.coefficients_poly[i];
+			  	first_co = this.coefficients_poly[i];
 			}
 			if(i < length2){
 				second_co = second_poly.coefficients_poly[i];
@@ -59,10 +183,45 @@ public class Polynomial{
 		
 	}
 	
+	public void saveToFile(String fileString){
+		try(FileWriter writerObj = new FileWriter(fileString)){
+			for(int i = 0; i < coefficients_poly.length; i++){
+				double current_coefficient = coefficients_poly[i];
+				int current_expo = exponent[i];
+				if(i > 0 && current_coefficient > 0){
+					writerObj.write("+");
+				}
+				if(current_coefficient > 0 || current_coefficient < 0){
+					if(current_expo == 0){
+						writerObj.write(Double.toString(current_coefficient));
+					}
+					else{
+						if(current_coefficient > 1 || current_coefficient < 1){
+							writerObj.write(Double.toString(current_coefficient));
+						}
+						writerObj.write("x");
+						if(current_expo != 1){
+							writerObj.write(Integer.toString(current_expo));
+						}
+						
+					}
+					
+				}
+				
+				
+			}
+		}
+		catch(IOException exceptionObj1){
+			System.err.println("Error in editing the file " + exceptionObj1.getMessage());
+		}
+		
+	}
+	
+	
 	public double evaluate(double x_val){
 		double output = 0.0;
 		for(int i =0; i < coefficients_poly.length; i++){
-			output = output + coefficients_poly[i] * Math.pow(x_val, i);
+			output = output + coefficients_poly[i] * Math.pow(x_val, exponent[i]);
 		}
 		return output;
 		
